97e7b11fa6d6499239a0d45b352364f5996baf21
Rahul <rahulgaur0304@gmail.com>
2022-01-11 15:04:07 +0530

    Second Commit

--- jobs/settings.py
+++ jobs/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=t&58zq6amb9**+$6=yli$0*hl#96$i9ojt84_xuv_)o(u9^s+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jobbsforyou.herokuapp.com']
 
 
 # Application definition
@@ -129,6 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
